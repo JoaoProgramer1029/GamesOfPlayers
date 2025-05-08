671e59540006d886b8446e864d47cdd936a48efc
JoaoProgramer1029 <joaodavidbiz@gmail.com>
2025-05-08 16:07:24 +0100

    2

---
+++ Novo.js
@@ -0,0 +1,7 @@
+const Título = document.getElementById("Título")
+function Português(){
+    Título.textContent = "Bem Vindo à <strong>GameOfPlayers</strong>"
+}
+function Inglês(){
+    Título.textContent = "Welcome to <strong>GameOfPlayers</strong>"
+}
